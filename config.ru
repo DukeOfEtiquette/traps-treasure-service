2e0f3ecf65c76eb8b119d5a8edbafca5f3b2426b
Adam DuQuette <duquetteadam@gmail.com>
2017-05-17 17:28:58 -0700

    Move redis connection to config file

--- config.ru
+++ config.ru
@@ -1,2 +1,8 @@
 require_relative 'main'
+
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 run Sinatra::Application
--- main.rb
+++ main.rb
@@ -1,27 +1,4 @@
 require 'sinatra'
-require 'redis'
-
-begin
-  encoded_url = URI.encode(ENV["REDISTOGO_URL"].to_s)
-  uri = URI.parse(encoded_url)
-  puts "HOST: #{uri.host}"
-  puts "PORT: #{uri.port}"
-  puts "PASS: #{uri.password}"
-  redis = Redis.new(
-    :host => uri.host,
-    :port => uri.port,
-    :password => uri.password
-  )
-#  puts redis.info
-  client_ping = redis.ping
-  if client_ping
-    puts 'CONNECTED!'
-  else
-    raise 'Ping failed'
-  end
-rescue => e
-  puts "Error: #{e}"
-end
 
 get '/' do
   message = "hello"
